f369343fef703241bf0406bea8de66cd1d4c2b16
Abhigyan <Abhigyan@example.com>
2020-11-06 22:17:12 -0600

    Adding new class

---
+++ Comp_Coding/First_Last_PositionOfElement_inSortedArray.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Comp_Coding
+{
+    class First_Last_PositionOfElement_inSortedArray
+    {
+        public int[] SearchRange(int[] nums, int target)
+        {
+            if (nums == null || nums.Length == 0)
+                return new int[] { -1, -1 };
+
+            int left = BinarySearch_left(nums, 0, nums.Length - 1, target);
+            int right = BinarySearch_right(nums, 0, nums.Length - 1, target);
+
+            return new int[] { left, right };
+        }
+
+        public int BinarySearch_left(int[] nums, int start, int end, int target)
+        {
+            if (start > end)
+                return -1;
+
+            int mid = start + (end - start) / 2;
+
+            if(nums[mid] == target)
+            {
+                if(mid - 1 >= start && nums[mid-1] == target)
+                {
+                    return BinarySearch_left(nums, start, mid - 1, target);
+                }
+                else
+                {
+                    return mid;
+                }
+            }
+            else if(nums[mid] < target)
+            {
+                return BinarySearch_left(nums, mid + 1, end, target);
+            }
+            else
+            {
+                return BinarySearch_left(nums, start, mid - 1, target);
+            }
+        }
+
+        public int BinarySearch_right(int[] nums, int start, int end, int target)
+        {
+            if (start > end)
+                return -1;
+
+            int mid = start + (end - start) / 2;
+
+            if(nums[mid] == target)
+            {
+                if(mid+1 <= end && nums[mid+1] == target)
+                {
+                    return BinarySearch_right(nums, mid + 1, end, target);
+                }
+                else
+                {
+                    return mid;
+                }
+            }
+
+            if(nums[mid] > target)
+            {
+                return BinarySearch_right(nums, start, mid - 1, target);
+            }
+            else
+            {
+                return BinarySearch_right(nums, mid + 1, end, target);
+            }
+        }
+    }
+}
--- Comp_Coding/Program.cs
+++ Comp_Coding/Program.cs
@@ -6,7 +6,8 @@ namespace Comp_Coding
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            First_Last_PositionOfElement_inSortedArray f = new First_Last_PositionOfElement_inSortedArray();
+            f.SearchRange(new int[] { 4, 4, 6, 7, 9, 9, 15 }, 9);
         }
     }
 }
