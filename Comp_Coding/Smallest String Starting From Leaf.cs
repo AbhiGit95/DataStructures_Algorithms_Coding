030b99c944a5d297ccbf5dfd532a6447193c66d4
Abhigyan <Abhigyan@example.com>
2021-04-21 13:00:45 -0500

    [ADD] : Tree Related Problems

---
+++ Comp_Coding/Smallest String Starting From Leaf.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Comp_Coding
+{
+    class Smallest_String_Starting_From_Leaf
+    {
+        List<string> list;
+        public string SmallestFromLeaf(TreeNode root)
+        {
+            if (root == null)
+                return "";
+
+            list = new List<string>();
+
+            StringBuilder sb = new StringBuilder();
+            Stack<char> stack = new Stack<char>();
+
+            DFS(root, sb);
+
+            list.Sort();
+
+            return list[0];
+        }
+
+        public void DFS(TreeNode node, StringBuilder sb)
+        {
+            if (node == null)
+                return;
+
+            sb.Insert(0,Convert.ToChar(node.val +'a'));
+
+            if (node.left == null && node.right == null)
+            {
+                var temp = sb.ToString();
+                list.Add(temp);
+            }
+
+            DFS(node.left, sb);
+            DFS(node.right, sb);
+
+            sb.Remove(0, 1);
+        }
+    }
+}
---
+++ Comp_Coding/Sum Root to Leaf Numbers.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Comp_Coding
+{
+    class Sum_Root_to_Leaf_Numbers
+    {
+        List<string> list;
+        public int SumNumbers(TreeNode root)
+        {
+            if (root == null)
+                return 0;
+
+            list = new List<string>();
+
+            StringBuilder sb = new StringBuilder();
+
+            DFS(root, sb);
+
+            int res = 0;
+
+            foreach(var v in list)
+            {
+                res += Convert.ToInt32(v);
+            }
+
+            return res;
+        }
+
+
+        public void DFS(TreeNode node, StringBuilder sb)
+        {
+            if (node == null)
+                return;
+
+            sb.Append(node.val);
+
+            if (node.left == null && node.right == null)
+            {
+                list.Add(sb.ToString());
+            }
+
+            DFS(node.left, sb);
+            DFS(node.right, sb);
+
+            sb.Remove(sb.Length, 1);
+        }
+    }
+}
